c12943a1741e62de400fdbbbf32ea9775c6d39b8
shibata <shibata@jidaikobo.com>
2016-10-04 19:18:30 +0900

    add Insert specific serial number. thx http://qiita.com/ShingoFukuyama/items/3ad7e24cb2d8f55b4cc5

--- jidaikobo.init.el
+++ jidaikobo.init.el
@@ -581,9 +581,35 @@
     ("S"   . 'mc/skip-to-previous-like-this)
     ("*"   . 'mc/mark-all-like-this)
     ("d"   . 'mc/mark-all-like-this-dwim)
-    ("i"   . 'mc/insert-numbers)
+    ("i"   . 'my/mc/insert-numbers)
     ("o"   . 'mc/sort-regions)
     ("O"   . 'mc/reverse-regions)))
+
+;;; Insert specific serial number
+;; thx http://qiita.com/ShingoFukuyama/items/3ad7e24cb2d8f55b4cc5
+(defvar my/mc/insert-numbers-hist nil)
+(defvar my/mc/insert-numbers-inc 1)
+(defvar my/mc/insert-numbers-pad "%01d")
+
+(defun my/mc/insert-numbers (start inc pad)
+  "Insert increasing numbers for each cursor specifically.  START, INC, PAD."
+  (interactive
+   (list (read-number "Start from: " 0)
+         (read-number "Increment by: " 1)
+         (read-string "Padding (%01d): " nil my/mc/insert-numbers-hist "%01d")))
+  (setq mc--insert-numbers-number start)
+  (setq my/mc/insert-numbers-inc inc)
+  (setq my/mc/insert-numbers-pad pad)
+  (mc/for-each-cursor-ordered
+   (mc/execute-command-for-fake-cursor
+    'my/mc--insert-number-and-increase
+    cursor)))
+
+(defun my/mc--insert-number-and-increase ()
+  "Insert number and increase."
+  (interactive)
+  (insert (format my/mc/insert-numbers-pad mc--insert-numbers-number))
+  (setq mc--insert-numbers-number (+ mc--insert-numbers-number my/mc/insert-numbers-inc)))
 
 ;;; ------------------------------------------------------------
 ;;; よく使うところに早く移動
