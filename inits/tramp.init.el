24ee0ab0b904bc25109370df4be978d96fafba05
jidaikobo-shibata <shibata@jidaikobo.com>
2026-09-15 13:12:05 +0900

    Prevent remote editing artifacts

--- inits/tramp.init.el
+++ inits/tramp.init.el
@@ -6,9 +6,17 @@
 (require 'ange-ftp)
 (require 'tramp)
 
-;; TRAMPでは自動バックアップしない
-(add-to-list 'backup-directory-alist
-             (cons tramp-file-name-regexp nil))
+;; TRAMPではバックアップとロックファイルをリモート側に作らない
+(defun my/backup-enable-for-local-file-p (file)
+  "Return non-nil when FILE is local and eligible for backup."
+  (and (not (file-remote-p file))
+       (normal-backup-enable-predicate file)))
+
+(setq backup-enable-predicate #'my/backup-enable-for-local-file-p)
+(setq remote-file-name-inhibit-locks t)
+
+;; TRAMPの自動保存ファイルはローカルの専用ディレクトリに置く
+(setq tramp-auto-save-directory my/auto-save-directory)
 
 ;; TRAMP ファイルでは trash を使わない
 (add-hook 'dired-mode-hook
